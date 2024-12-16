e44e84cc08abce63fff665bfb585a9de312ed403
khadeejab <f2022-038@bnu.edu.pk>
2024-12-16 22:07:33 +0500

    singup / signin pages

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,36 +2,155 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebasebackend/snack_bar_helper.dart';
 import 'package:flutter/material.dart';
 
+import 'modal2.dart';
 import 'signin_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Page'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.logout),
-            onPressed: () async {
-              try {
-                await FirebaseAuth.instance.signOut();
-                showSnackBar(context, 'Logged out successfully!', Colors.green);
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => SignInScreen()),
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                showModalBottomSheet(
+                  context: context,
+                  isScrollControlled: true, // Allows the modal to define its height
+                  shape: const RoundedRectangleBorder(borderRadius: BorderRadius.vertical(top: Radius.circular(50)),),
+                  builder: (BuildContext context) {
+                    return Container(
+                      decoration: const BoxDecoration(
+                        gradient: RadialGradient(
+                          center: Alignment.center,
+                          radius: 1.4,
+                          colors: [
+                            Color(0xFFCCEFFF),
+                            Color(0xFFEFF9F2),
+                            Color(0xFFCFCFCF),
+                          ],
+                          stops: [0.3, 0.8, 1],
+                        ),
+                      ),
+
+                      height: MediaQuery.of(context).size.height * 0.9,
+                      width: double.infinity,
+                      padding: const EdgeInsets.only( top: 20.0),
+
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              SizedBox(width: 20,),
+                              ElevatedButton(
+                                onPressed: () {
+                                  //calendar top left area!!!
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30.0), // Adjust radius as needed
+                                  ),
+                                  padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+                                ),
+                                child: Row(
+                                  children: [
+                                    Text('Sun, 4 Jun'),
+                                    SizedBox(width: 10),
+                                    Icon(Icons.calendar_month_outlined),],),
+                              ),
+
+                              SizedBox(width: 40,),
+
+                              Text("1/4"),
+
+                              SizedBox(width: 80,),
+
+                              ElevatedButton(
+                                onPressed: () {
+                                  Navigator.pop(context); // Close the modal sheet
+                                },
+                                child: const Icon(Icons.close, color: Colors.black,),
+                                style: ElevatedButton.styleFrom(
+                                  shape: const CircleBorder(),
+                                  padding: const EdgeInsets.all(6.0),
+                                ),
+                              ),
+
+                            ],
+                          ),
+
+                          SizedBox(height: 30),
+
+                          Text("Whats your mood right now?", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
+                          SizedBox(height: 30),
+
+                          SizedBox(width: 280,
+                            child: Text("Select mood that reflects the most how you are feeling at this moment.",
+                                style: TextStyle(fontSize: 16, ),
+                                textAlign: TextAlign.center),
+                          ),
+
+                          SizedBox(height: 180),
+
+                          SizedBox(
+                            width: double.infinity,
+                            height: 100,
+                            child: Stack(
+                              children: [
+                                Image.asset('lib/assets/emojibar-def.png'),
+                                // Positioned(
+                                //   left:165,
+                                //   child: IconButton(onPressed: (){}, icon: Image.asset('lib/assets/halo.png', height: 60,)),
+                                // )
+                              ],
+                            ),
+                          ),
+
+                          SizedBox(height: 200),
+
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Color(0xFF8B4CFC),
+                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40),),
+                              padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+                              minimumSize: const Size(350, 20),
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const TwoByFour(),
+                                ),
+                              );
+
+                              // Navigator.pop(context); // Close the current modal
+                              // showModalBottomSheet(
+                              //   context: context,
+                              //   isScrollControlled: true,
+                              //   shape: const RoundedRectangleBorder(
+                              //     borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+                              //   ),
+                              //   builder: (BuildContext context) {
+                              //     return TwoByFour();
+                              //   },
+                              // );
+                            },
+                            child: const Text(
+                              'Continue',
+                              style: TextStyle(fontSize: 18, color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
                 );
-              } catch (e) {
-                showSnackBar(context, 'Logout failed', Colors.red);
-              }
-            },
-          ),
-        ],
-      ),
-      body: Center(
-        child: Text(
-          'Welcome to the Home Page!',
-          style: TextStyle(fontSize: 24),
+              },
+              child: Text('Show Modal Bottom Sheet'),
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/modal4.dart
@@ -0,0 +1,205 @@
+import 'package:firebasebackend/home_screen.dart';
+import 'package:flutter/material.dart';
+
+
+class modal4 extends StatefulWidget {
+  const modal4({super.key});
+
+  @override
+  State<modal4> createState() => _modal4State();
+}
+
+class _modal4State extends State<modal4> {
+  TextEditingController notesController = TextEditingController();
+
+  void showPopupDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left:20.0),
+                  child: Image.asset('lib/assets/goodtogo.png'),
+                ),
+
+                const Text(
+                  "You're on a good way!\nYour day is going \n amazing",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  "Keep tracking your mood to know how to improve your mental health.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 14, color: Colors.grey),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF8B4CFC),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    minimumSize: const Size(200, 50),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context); // Close the popup
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen()));
+                  },
+                  child: const Text(
+                    'Got it',
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: RadialGradient(
+              center: Alignment.center,
+              radius: 1.4,
+              colors: [
+                Color(0xFFCCEFFF),
+                Color(0xFFEFF9F2),
+                Color(0xFFCFCFCF),
+              ],
+              stops: [0.3, 0.8, 1],
+            ),
+          ),
+          child: Column(
+            children: [
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.arrow_back),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  const Text("4/4", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                  IconButton(
+                    icon: const Icon(Icons.close),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 20),
+
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Text(
+                  "Any thing you want to add",
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 50.0),
+                child: Text(
+                  "Add your notes on any thought that relates to your mood",
+                  style: TextStyle(fontSize: 14, color: Colors.black),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Container(
+                  height: 300,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50.0),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 40,
+                      )
+                    ],
+                  ),
+                  child: TextField(
+                    controller: notesController,
+                    maxLines: 15,
+                    decoration: InputDecoration(
+                      hintText: "Write your notes here",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+
+              const Spacer(),
+
+              Column(
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFF8B4CFC),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40),
+                      ),
+                      minimumSize: const Size(350, 50),
+                    ),
+                    onPressed: showPopupDialog,
+                    child: const Text(
+                      'Save',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
+
+                  const SizedBox(height: 5),
+
+                  TextButton(
+                    onPressed: showPopupDialog,
+                    child: const Text(
+                      'Skip and Save',
+                      style: TextStyle(fontSize: 16, color: Color(0xFF8B4CFC)),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
