90db72bef8bad83484a6293e17c1dd294058d1a8
mishajehangir <f2024-0069@bnu.edu.pk>
2024-12-26 22:28:45 +0500

    continue buttons on addMood_page2 and addMood_page3 are now floating (implemented stack).

--- lib/addMood_page2.dart
+++ lib/addMood_page2.dart
@@ -55,236 +55,194 @@ class _AddEmotionsState extends State<AddEmotions> {
 
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          height: MediaQuery.of(context).size.height, // Full screen height
-          decoration: const BoxDecoration(
-            gradient: RadialGradient(
-              center: Alignment.center,
-              radius: 1.4,
-              colors: [
-                Color(0xFFCCEFFF),
-                Color(0xFFEFF9F2),
-                Color(0xFFCFCFCF),
-              ],
-              stops: [0.3, 0.8, 1],
-            ),
-          ),
-          child: SingleChildScrollView( // Allows scrolling if content exceeds the screen
-            child: Column(
-              children: [
-                Row(
+        child: Stack(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height, // Full screen height
+              decoration: const BoxDecoration(
+                gradient: RadialGradient(
+                  center: Alignment.center,
+                  radius: 1.4,
+                  colors: [
+                    Color(0xFFCCEFFF),
+                    Color(0xFFEFF9F2),
+                    Color(0xFFCFCFCF),
+                  ],
+                  stops: [0.3, 0.8, 1],
+                ),
+              ),
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.only(bottom: 100), // Padding to avoid button overlap
+                child: Column(
                   children: [
-                    const SizedBox(width: 20),
-                    BackButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
+                    Row(
+                      children: [
+                        const SizedBox(width: 20),
+                        BackButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                        const SizedBox(width: 130),
+                        const Text("2/4"),
+                        const SizedBox(width: 100),
+                        ElevatedButton(
+                          onPressed: () {
+                            moodProvider.clear();
+                            Navigator.pushAndRemoveUntil(
+                              context,
+                              MaterialPageRoute(builder: (context) => HomeScreen()),
+                                  (route) => false,
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            shape: const CircleBorder(),
+                            padding: const EdgeInsets.all(6.0),
+                          ),
+                          child: const Icon(Icons.close, color: Colors.black),
+                        ),
+                      ],
                     ),
-                    const SizedBox(width: 130),
-                    const Text("2/4"),
-                    const SizedBox(width: 100),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushAndRemoveUntil(
-                          context,
-                          MaterialPageRoute(builder: (context) => HomeScreen()),
-                              (route) => false,
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        shape: const CircleBorder(),
-                        padding: const EdgeInsets.all(6.0),
+
+                    const SizedBox(height: 30),
+
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        "Choose the emotions that make you feel neutral",
+                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
                       ),
-                      child: const Icon(Icons.close, color: Colors.black),
                     ),
+
+                    const SizedBox(height: 10),
+
+                    const SizedBox(
+                      width: 280,
+                      child: Text(
+                        "Select at least 1 emotion",
+                        style: TextStyle(fontSize: 16),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+
+                    const SizedBox(height: 20),
+
+                    // Search Bar
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: TextField(
+                        onChanged: (value) {
+                          setState(() {
+                            searchQuery = value;
+                          });
+                        },
+                        decoration: InputDecoration(
+                          labelText: '  Search emotions',
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          suffixIcon: const Icon(Icons.search),
+                        ),
+                      ),
+                    ),
+
+                    const SizedBox(height: 10),
+
+                    if (moodProvider.selectedEmotions.isNotEmpty) ...[
+                      const Padding(
+                        padding: EdgeInsets.only(right: 200),
+                        child: Text(
+                          "Selected Emotions:",
+                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+
+                      Wrap(
+                        spacing: 12.0,
+                        children: moodProvider.selectedEmotions.map((emotionTitle) {
+                          // Find the corresponding emoji item by title
+                          final emojiItem = allEmotions.firstWhere((item) => item.title == emotionTitle);
+                          return Chip(
+                            label: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Image.asset(
+                                  emojiItem.imagePath,
+                                  width: 20,
+                                  height: 20,
+                                ),
+                                const SizedBox(width: 5),
+                                Text(
+                                  emotionTitle,
+                                  style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+                                ),
+                              ],
+                            ),
+                            backgroundColor: Colors.purple[200],
+                            deleteIcon: const Icon(Icons.close, size: 16, color: Colors.black),
+                            onDeleted: () {
+                              moodProvider.toggleEmotion(emotionTitle); // Deselect the emotion
+                            },
+                          );
+                        }).toList(),
+                      ),
+                    ],
+
+                    const SizedBox(height: 20),
+
+                    Wrap(
+                      spacing: 20.0,
+                      runSpacing: 8.0,
+                      children: filteredEmotions.map((emotion) {
+                        return GestureDetector(
+                          onTap: () {
+                            moodProvider.toggleEmotion(emotion.title); // Use the setter method
+                          },
+                          child: Column(
+                            children: [
+                              CircleAvatar(
+                                backgroundColor: moodProvider.selectedEmotions.contains(emotion.title)
+                                    ? Colors.purple[100]
+                                    : Colors.grey[300],
+                                radius: 40,
+                                child: Image.asset(
+                                  emotion.imagePath,
+                                  width: 35,
+                                  height: 35,
+                                ),
+                              ),
+                              Text(
+                                emotion.title,
+                                style: const TextStyle(fontSize: 12),
+                              ),
+                            ],
+                          ),
+                        );
+                      }).toList(),
+                    ),
+
+                    const SizedBox(height: 30),
                   ],
                 ),
+              ),
+            ),
 
-                const SizedBox(height: 30),
-
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    "Choose the emotions that make you feel neutral",
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-
-                const SizedBox(height: 10),
-
-                const SizedBox(
-                  width: 280,
-                  child: Text(
-                    "Select at least 1 emotion",
-                    style: TextStyle(fontSize: 16),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-
-                const SizedBox(height: 20),
-
-                // Search Bar
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: TextField(
-                    onChanged: (value) {
-                      setState(() {
-                        searchQuery = value;
-                      });
-                    },
-                    decoration: InputDecoration(
-                      labelText: '  Search emotions',
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30.0),
-                      ),
-                      suffixIcon: const Icon(Icons.search),
-                    ),
-                  ),
-                ),
-
-                const SizedBox(height: 10),
-
-                if (moodProvider.selectedEmotions.isNotEmpty) ...[
-                  const Padding(
-                    padding: EdgeInsets.only(right: 200),
-                    child: Text(
-                      "Selected Emotions:",
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-
-                  Wrap(
-                    spacing: 12.0,
-                    children: moodProvider.selectedEmotions.map((emotionTitle) {
-                      // Find the corresponding emoji item by title
-                      final emojiItem = allEmotions.firstWhere((item) => item.title == emotionTitle);
-                      return Chip(
-                        label: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Image.asset(
-                              emojiItem.imagePath,
-                              width: 20,
-                              height: 20,
-                            ),
-                            const SizedBox(width: 5),
-                            Text(
-                              emotionTitle,
-                              style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
-                            ),
-                          ],
-                        ),
-                        backgroundColor: Colors.purple[200],
-                        deleteIcon: const Icon(Icons.close, size: 16, color: Colors.black),
-                        onDeleted: () {
-                          moodProvider.toggleEmotion(emotionTitle); // Deselect the emotion
-                        },
-                      );
-                    }).toList(),
-                  ),
-                ],
-
-
-                SizedBox(height: 20,),
-
-                const Padding(
-                  padding: EdgeInsets.only(right: 200),
-                  child: Text(
-                    "Recently Emotions:",
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-                  ),
-                ),
-
-                const SizedBox(height: 10),
-
-                Wrap(
-                  spacing: 20.0,
-                  runSpacing: 8.0,
-                  children: filteredEmotions.map((emotion) {
-                    return GestureDetector(
-                      onTap: () {
-                        moodProvider.toggleEmotion(emotion.title); // Use the setter method
-                      },
-                      child: Column(
-                        children: [
-                          CircleAvatar(
-                            backgroundColor: moodProvider.selectedEmotions.contains(emotion.title)
-                                ? Colors.purple[100]
-                                : Colors.grey[300],
-                            radius: 40,
-                            child: Image.asset(
-                              emotion.imagePath,
-                              width: 35,
-                              height: 35,
-                            ),
-                          ),
-                          Text(
-                            emotion.title,
-                            style: const TextStyle(fontSize: 12),
-                          ),
-                        ],
-                      ),
-                    );
-                  }).toList(),
-                ),
-
-                const SizedBox(height: 30),
-
-                const Padding(
-                  padding: EdgeInsets.only(right: 240),
-                  child: Text(
-                    "All Emotions:",
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-                  ),
-                ),
-
-                const SizedBox(height: 20),
-
-                Wrap(
-                  spacing: 20.0,
-                  runSpacing: 8.0,
-                  children: filteredEmotions.map((emotion) {
-                    return GestureDetector(
-                      onTap: () {
-                        moodProvider.toggleEmotion(emotion.title); // Use the setter method
-                      },
-                      child: Column(
-                        children: [
-                          CircleAvatar(
-                            backgroundColor: moodProvider.selectedEmotions.contains(emotion.title)
-                                ? Colors.purple[100]
-                                : Colors.grey[300],
-                            radius: 40,
-                            child: Image.asset(
-                              emotion.imagePath,
-                              width: 35,
-                              height: 35,
-                            ),
-                          ),
-                          Text(
-                            emotion.title,
-                            style: const TextStyle(fontSize: 12),
-                          ),
-                        ],
-                      ),
-                    );
-                  }).toList(),
-                ),
-
-                SizedBox(height: 20,),
-
-                ElevatedButton(
+            // Floating "Continue" button
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF8B4CFC),
+                    backgroundColor: const Color(0xFF8B4CFC),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(40),
                     ),
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 40.0, vertical: 15.0),
+                    padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 15.0),
                     minimumSize: const Size(350, 10),
                   ),
                   onPressed: () {
@@ -312,12 +270,9 @@ class _AddEmotionsState extends State<AddEmotions> {
                     style: TextStyle(fontSize: 18, color: Colors.white),
                   ),
                 ),
-
-                SizedBox(height: 20,),
-
-              ],
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
