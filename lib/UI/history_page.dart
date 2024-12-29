ae85ef21a94bdc6f02beb6126b74e4d313d83e97
mishajehangir <f2024-0069@bnu.edu.pk>
2024-12-29 22:12:24 +0500

    edit mood entry completely working now. all fields are editable. UI still needs major work. Refactored code to create date and time picker widget

--- lib/UI/history_page.dart
+++ lib/UI/history_page.dart
@@ -197,13 +197,13 @@ class _HistoryTileState extends State<HistoryTile> {
 
   @override
   Widget build(BuildContext context) {
-    String displayedNote = widget.note.length > 20
-        ? widget.note.substring(0, 20) // Display first 20 characters
+    String displayedNote = widget.note.length > 40
+        ? widget.note.substring(0, 40) // Display first 20 characters
         : widget.note; // If note is less than or equal to 20 characters, display full note
 
-    bool showReadMore = widget.note.length > 20; // Only show Read more if note > 20 characters
+    bool showReadMore = widget.note.length > 40; // Only show Read more if note > 20 characters
 
-    if (_isNoteExpanded && widget.note.length > 20) {
+    if (_isNoteExpanded && widget.note.length > 40) {
       displayedNote = widget.note; // Show the full note if expanded
     }
 
