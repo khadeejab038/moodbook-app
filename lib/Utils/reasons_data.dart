e3bd55902b1d6aaaa59c2d67e64cd1dd3374fdb6
mishajehangir <f2024-0069@bnu.edu.pk>
2024-12-30 11:48:11 +0500

    updated the list of available reasons

--- lib/Utils/reasons_data.dart
+++ lib/Utils/reasons_data.dart
@@ -1,5 +1,11 @@
 final List<String> allReasons = [
-  "Work", "Hobbies", "Family", "Breakup", "Weather", "Wife",
-  "Party", "Love", "Self esteem", "Sleep", "Social", "Food",
-  "Distant", "Content", "Exams"
+  "Work", "Family", "Relationship", "Achievement", "Sleep", "Finances",
+  "Friendship", "Health", "Stress", "Motivation", "Feedback", "Deadlines",
+  "Appreciation", "Weather", "Growth", "Loneliness", "Comparison", "Exercise",
+  "Relaxation", "Productivity", "Anxiety", "Kindness", "Conflict", "Loss",
+  "Expectations", "Celebration", "Entertainment", "Hobby", "Schedule", "Support",
+  "Compliments", "Accomplishment", "Responsibilities", "Travel", "Nostalgia",
+  "Obligations", "Disconnection", "Setbacks", "Compassion", "Pet", "Guilt",
+  "Confidence", "Belonging", "Routine", "Milestone", "Coworkers", "Commute",
+  "Learning", "Time", "Communication"
 ];
